f0a25e31c0e744fe1bdcb1ae922ce6d19a8168c0
Tien Dat Pham <thisisdat1998@gmail.com>
2024-12-16 16:35:33 +0700

    update server

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -1663,7 +1663,7 @@ const __default__ = proxy.default;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [697,605,837,461], () => (__webpack_exec__(9737)));
+var __webpack_exports__ = __webpack_require__.X(0, [697,605,837,375], () => (__webpack_exec__(9737)));
 module.exports = __webpack_exports__;
 
 })();
